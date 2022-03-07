9a5562d9e47f6e8d9941b7afcf47447a2780f14b
Steve Lawrence <steve.lawrence@noaa.gov>
2022-03-07 17:42:44 +0000

    fix: modify stack to support p7c version of ufs-weather-model
    body: In order to support the p7c version of the ufs, we had to
    modify several supporting library versions as well as the config
    for hera.

--- config/config_hera.sh
+++ config/config_hera.sh
@@ -2,14 +2,14 @@
 
 # Compiler/MPI combination
 export HPC_COMPILER="intel/18.0.5.274"
-export HPC_MPI="impi/2018.0.4"
-export HPC_PYTHON="miniconda3/4.6.14"
+export HPC_MPI="impi/2018.4.274"
+export HPC_PYTHON="miniconda3/3.7.3"
 
 # Build options
 export USE_SUDO=N
 export PKGDIR=pkg
 export LOGDIR=log
-export OVERWRITE=N
+export OVERWRITE=Y
 export NTHREADS=8
 export   MAKE_CHECK=N
 export MAKE_VERBOSE=N
@@ -17,15 +17,16 @@ export   MAKE_CLEAN=N
 export DOWNLOAD_ONLY=N
 export STACK_EXIT_ON_FAIL=Y
 export WGET="wget -nv"
-export VENVTYPE="condaenv"
+# export VENVTYPE="condaenv"
 
 # Load these basic modules for Hera
-module purge
 module load cmake/3.20.1
+module load intel/18.0.5.274
+module load impi/2018.4.274
 
 # Build FMS with AVX2 flags
 export STACK_fms_CFLAGS="-march=core-avx2"
 export STACK_fms_FFLAGS="-march=core-avx2"
 
 # Miniconda3 URL on Hera
-export STACK_miniconda3_URL="http://anaconda.rdhpcs.noaa.gov"
+#export STACK_miniconda3_URL="http://anaconda.rdhpcs.noaa.gov"
